1e81e551131439569685491c025ce87f43208633
Sebastian Moruszewicz <sdm6@illinois.edu>
2021-05-07 14:16:40 -0500

    Last non-working commit

--- graphics_processor.sv
+++ graphics_processor.sv
@@ -14,7 +14,8 @@ module graphics_processor(
 
 	logic gpu_clk_150;
 	logic reset, raster_start, user_key;
-	logic [31:0] p1[3], p2[3], p3[3];
+	logic [31:0] p1[3], p2[3], p3[3], p4[3], p5[3], p6[3];
+	logic [31:0] pru1[3], pru2[3], pru3[3];
 	logic raster_done;
 	logic [9:0] fb_x, fb_y;
 	logic [3:0] fb_data;
@@ -37,7 +38,7 @@ module graphics_processor(
 		.clk(MAX10_CLK1_50),
 		.areset(reset),
 		.start(raster_start),
-		.p1(p1), .p2(p2), .p3(p3),
+		.p1(pru1), .p2(pru2), .p3(pru3),
 		.done(raster_done),
 		.fb_x(fb_x), .fb_y(fb_y),
 		.fb_data(fb_data),
@@ -78,6 +79,19 @@ module graphics_processor(
 		p3[0] = 32'h43290000; // (169.0, 69.0, 1.0)
 		p3[1] = 32'h428a0000;
 		p3[2] = 32'h3f800000;
+
+		// Constant points
+		p4[0] = 32'h438c0000; // (280.0, 69.0, 1.0)
+		p4[1] = 32'h328a0000;
+		p4[2] = 32'h3f800000;
+
+		p5[0] = 32'h42280000; // (42.0, 69.0, 1.0)
+		p5[1] = 32'h328a0000;
+		p5[2] = 32'h3f800000;
+
+		p6[0] = 32'h42600000; // (56.0, 169.0, 1.0)
+		p6[1] = 32'h33290000;
+		p6[2] = 32'h3f800000;
 	end
 
 	always_ff @( posedge MAX10_CLK1_50 ) begin : UPDATE_STATE_VAR
@@ -147,6 +161,19 @@ module graphics_processor(
 			SWAP_BUFFERS : begin
 				raster_start = 1'b0;
 			end
+		endcase
+
+		case (buffer_num)
+			1'b0 : begin
+					pru1 = p1;
+					pru2 = p2;
+					pru3 = p3;
+			end
+			1'b1 : begin
+					pru1 = p4;
+					pru2 = p5;
+					pru3 = p6;
+			end 
 		endcase
 	end
 
